7f9bf3352ec9102295bb97fcd3abfe079bb73f4a
cahyof30 <f30.cahyo@gmail.com>
2023-05-08 10:49:04 +0700

    Tugas Pertemuan 5 Mobile Programming

---
+++ lib/ui/menu_page.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import '../model/pegawai.dart';
+import 'pegawai_detail.dart';
+import 'pegawai_page.dart';
+import '../model/pasien.dart';
+import 'pasien_detail.dart';
+import 'pasien_page.dart';
+
+class MenuPage extends StatefulWidget {
+  const MenuPage({super.key});
+
+  @override
+  State<MenuPage> createState() => _MenuPageState();
+}
+
+class _MenuPageState extends State<MenuPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Menu App")),
+      body: GridView.count(
+        crossAxisCount: 1,
+        childAspectRatio: 2,
+        children: [
+          GestureDetector(
+            child: CustomCard(
+                title: "Data Pegawai", image: "images/3641634cropped.jpg"),
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => PegawaiPage()));
+            },
+          ),
+          GestureDetector(
+            child: CustomCard(
+                title: "Data Pasien", image: "images/4061889cropped.png"),
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => PasienPage()));
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomCard extends StatelessWidget {
+  CustomCard({required this.title, required this.image});
+
+  String title;
+  String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: Card(
+        //menambahkan bayangan
+        elevation: 5,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 120,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(
+                        image,
+                      ),
+                      fit: BoxFit.fitWidth)),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(title),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
